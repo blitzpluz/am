cf945a9c2c880492f578c5ad56726a154f117273
marcelo paolo paez aracena <marcelo.paez.15@alumnos.uda.cl>
2019-02-10 11:38:01 -0300

    correccion archivos estatic(MEDIA_ROOT) y agregando boostrap a static

--- emi/urls.py
+++ emi/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from osi import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^contact/$', views.contact, name='contact'),
 ]
 if settings.DEBUG:
