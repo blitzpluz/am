1aa8adb534446ba43e238e335cabcb0a2b89709f
marcelo paolo paez aracena <marcelo.paez.15@alumnos.uda.cl>
2019-02-18 23:05:39 -0300

    personalizando panel admin

--- osi/admin.py
+++ osi/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 
 from .models import formulario,tecnico
 
-
+admin.site.site_header ='MESA DE AYUDA INFORMATICA 2019" '
 class personalizar(admin.ModelAdmin):
     list_display=["usuario","solicitante","centro_de_costo","anexo","atendido_por","asunto","status_ticket","fecha_inicio_ticket"]
-    list_filter=["fecha_inicio_ticket","usuario","centro_de_costo","atendido_por","solicitante","status_ticket"]
-
+    list_filter=["fecha_inicio_ticket","atendido_por","status_ticket"]
+    search_fields=["usuario","solicitante","centro_de_costo","fecha_solucion_ticket"]
     class Meta:
         model= formulario,tecnico
 
--- osi/forms.py
+++ osi/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    usuario = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'siga el ejemplo: P.Rivera'}))
-    solicitante = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'rellenar en caso de que no sea para usted la atencion'}))
+    usuario = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'inicial de su nombre.apellido ej: P.Rivera'}))
+    solicitante = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'rellenar con su Nombre.Apellido si solicita para otro la atencion'}))
 
     centro_de_costo = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'ejemplo : 01-1020'}))
     anexo = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':'su anexo telefonico'}))
-    asunto = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder':'explique lo que necesita  y sea conciso'}))
+    asunto = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control','placeholder':'explique lo que necesita'}))
